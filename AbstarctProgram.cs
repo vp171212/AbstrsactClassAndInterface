1e91d43488135e5f4a761c2cf26e64901f4f51e1
Ved Prakash <91253410+vp171212@users.noreply.github.com>
2023-08-21 09:29:59 +0530

    Add files via upload

---
+++ AbstarctProgram.cs
@@ -0,0 +1,23 @@
+﻿using AbstractClassDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractChild operation = new AbstractChild();
+            AbstractParent parent = operation;
+
+            parent.Add(10, 100);
+            parent.Sub(100, 10);
+            parent.Multiplication(10, 100);
+            parent.Divide(100, 5);
+        }
+    }
+}
