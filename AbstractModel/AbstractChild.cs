eaabbeef2725cbf88edcb6d83d864c7b3d637f81
Ved Prakash <91253410+vp171212@users.noreply.github.com>
2023-08-21 09:25:37 +0530

    Add files via upload

---
+++ AbstractModel/AbstractChild.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassDemo.Model
+{
+    internal class AbstractChild:AbstractParent
+    {
+        public override void Multiplication(int x,int y)
+        {
+            Console.WriteLine($"Product is: {x * y}");
+        }
+        public override void Divide(int x,int y)
+        {
+            Console.WriteLine($"Division is: {x / y}");
+        }
+    }
+}
